3bdbb644cc5bb806fc4357735f8dedd369bea0f1
im_TaeHo_K <xogh8025@naver.com>
2022-01-20 23:17:23 +0900

    refactor Make vriable

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import { createStore } from "redux";
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
+const ADD = "ADD";
+const MINUS = "MINUS";
 
 number.innerText = 0;
 
 const counterModifier = (count = 0, action) => {
   switch (action.type) {
-    case "ADD":
+    case ADD:
       return count + 1;
-    case "MINUS":
+    case MINUS:
       return count - 1;
     default:
       return count;
@@ -25,10 +27,10 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
