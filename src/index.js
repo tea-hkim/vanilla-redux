4979af90bc86fadd6f3f2c55f1956d980331ff33
im_TaeHo_K <xogh8025@naver.com>
2022-01-23 13:10:50 +0900

    refactor Seperate createTodo for make component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
+const createToDo = (toDo) => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
+};
+
 const handleSubmit = (event) => {
   event.preventDefault();
   const toDo = input.value;
   input.value = "";
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+  createToDo(toDo);
 };
 
 form.addEventListener("submit", handleSubmit);
