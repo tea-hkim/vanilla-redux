6945062bd95c5ce16512acf7605e540f1c6ee589
im_TaeHo_K <xogh8025@naver.com>
2022-01-23 14:08:00 +0900

    feat Add Redux store and make add todo function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,43 @@
+import { createStore } from "redux";
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = (toDo) => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: action.id }];
+    case DELETE_TODO:
+      return;
+    default:
+      return;
+  }
+};
+
+const store = createStore(reducer);
+
+const createToDo = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    ul.appendChild(li);
+  });
 };
 
 const handleSubmit = (event) => {
   event.preventDefault();
+  const ID = Date.now();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo);
+  store.dispatch({ type: ADD_TODO, text: toDo, id: ID });
+  createToDo();
 };
 
 form.addEventListener("submit", handleSubmit);
